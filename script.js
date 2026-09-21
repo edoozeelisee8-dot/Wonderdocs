efd776cb5cdc7c6a73362803cd47ea67c05751ec
Eli'Ze <kine33zwonder@gmail.com>
2026-09-21 21:50:31 +0100

    cruciale

--- script.js
+++ script.js
@@ -108,14 +108,15 @@ const allowedMedicalKeywords = [
     'massage', 'etirement', 'étirement', 'renfocement', 'genou', 'epaule', 'colonne', 'vertebre',
     'scoliose', 'algodystrophie', 'sdrc', 'biomecanique', 'biomécanique', 'posture', 'statique',
     'bassin', 'membre', 'traitement', 'memoire', 'rapport', 'cours', 'stage','boiterie'
-    'innervation', 'système nerveux', 'innervation motrice', 'innervation sensitive',
+    'innèrvation', 'système nerveux', 'innervation motrice', 'innervation sensitive',
     'arthrose', 'mémoire', 'tendinite', 'arthrite','physiothérapie','vascularisation',
-    'Goniomètre', 'goniometre', 'amplitude articulaire', 'bilan articulaire', 'bilan kinészithérapique',
+    'Goniomètre', 'goniometre', 'amplitude articulaire', 'bilan articulaire',
     'neurone', 'syndrome canalaire', 'imagérie', 'imagérie médicale', 'vision', 'vue','paralysie',
     'gustation', 'olfaction', 'communication nerveuse', 'plexus brachial', 'physiologie renale',
-    'physiologie sanguine', 'physiologie musculaire', 'cruralgie', 'système nerveux', 'système nerveux autonome',
-    'système nerveux central', 'système nerveux périphérique', 'système nerveux sympathique', 'système nerveuxz parasympathique',
-
+    'physiologie sanguine', 'physiologie musculaire', 'cruralgie', 'système nerveux',
+    'système nerveux central', 'système nerveux périphérique', 'système nerveux sympathique', 
+    'système nerveuxz parasympathique','système nerveux autonome', 'bilan kinésithérapique'
+]
 /* ================================ ÉTAT =================================== */
 
 let articlesDatabase = [];
